bfc078e670e4669540b69603e578c8dd031faccb
Anshuman Bhoi <anshumanbhoi71@gmail.com>
2026-04-14 18:02:12 +0530

    deployment config add

--- src/api/axiosinstance.js
+++ src/api/axiosinstance.js
@@ -1,7 +1,8 @@
 
-import axios from "axios";
+import axios from "axios"
+
 export const axiosInstance = axios.create({
     baseURL: "https://sanctuary-backend-3m0s.onrender.com/",
-    // baseURL: "http://localhost:9080/",
+   //  baseURL: "http://localhost:9080/",
     withCredentials: true,
 });
