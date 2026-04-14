273f4e9d7f2dd949e81559faa08315d32d42a9e4
Anshuman Bhoi <anshumanbhoi71@gmail.com>
2026-04-14 15:57:08 +0530

    deployment config addeed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { routes } from './routes/routes'
 import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
   return (
